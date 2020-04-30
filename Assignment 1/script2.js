2b0d9942c4c1b41fa1958ab7853fb890a620d9f6
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-04-30 15:33:47 +0530

    Update script2.js

--- Assignment 1/script2.js
+++ Assignment 1/script2.js
@@ -43,7 +43,6 @@ function validate(){
     let email=document.getElementById("email").value;
     let pwd =document.getElementById("pwd").value;
     let error1=document.getElementById("error1");
-    let error2=document.getElementById("error2");
     
     let regexp=/^([A-Za-z0-9\.-]+)@([a-zA-Z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
     let regexp1=/^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!@#$%&*]+.{8,15})$/;
@@ -53,15 +52,15 @@ function validate(){
          return false;
         }
         
-                 if(!(regexp1.test(pwd))){
+                else if(!(regexp1.test(pwd))){
                   
-                    alert("Password should contain at least one number, one lower and one upper case letter"+pwd);
+                    alert("Password should contain at least one number, one lower and one upper case letter");
                     return false;
                 }
                 
-                    
+                 else{   
                     return true;
                
-            
+                 }
             }
     
